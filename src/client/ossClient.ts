678d64dc42100a4a9c222d1203bcb755d1b33081
Lvin Xu <lvin_xu@outlook.com>
2022-08-23 18:14:08 +0800

    update: update COS config

--- src/client/ossClient.ts
+++ src/client/ossClient.ts
@@ -1,21 +1,27 @@
 import COS = require('cos-nodejs-sdk-v5');
 
 const cos = new COS({
-  SecretId:
-    process.env.REACT_WEB_COS_SECRET_ID ||
-    'AKIDtnmngP4Wnpd7UgiWUHtJcntlOq8E8rNh',
-  SecretKey:
-    process.env.REACT_WEB_COS_SECRET_KEY || 'A6WpFrwwlVOAl9b1GPXxPjtqcLc4TiO5',
+  SecretId: process.env.REACT_WEB_COS_SECRET_ID || '',
+  SecretKey: process.env.REACT_WEB_COS_SECRET_KEY || '',
 });
 /* 自己封装的上传方法 */
 
 const myUpload = () => {
-  cos.putObject({
-    Body: '',
-    Bucket: '',
-    Region: '',
-    Key: '',
-  });
+  cos.putObject(
+    {
+      Bucket: 'xulf-oss-1256971899' /* 必须 */,
+      Region: 'ap-shanghai' /* 必须 */,
+      Key: 'exampleobject' /* 必须 */,
+      StorageClass: 'STANDARD',
+      Body: fs.createReadStream('./exampleobject'), // 上传文件对象
+      onProgress: function (progressData) {
+        console.log(JSON.stringify(progressData));
+      },
+    },
+    (err, data) => {
+      console.log(err || data);
+    }
+  );
 };
 
 /* 自己封装的删除方法 */
