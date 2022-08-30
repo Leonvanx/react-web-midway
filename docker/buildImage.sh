1e78d10d6c95d0137235f4d58c73eb0be011accb
Lvin Xu <lvin_xu@outlook.com>
2022-08-30 15:21:25 +0800

    build: update dockerignore file

--- docker/buildImage.sh
+++ docker/buildImage.sh
@@ -2,6 +2,6 @@ git checkout . && git pull
 
 npm run build & mv ./dist ./docker
 
-version_tag = `date +%Y.%m%d.%H.%M`
+# version_tag = `date +%Y.%m%d.%H.%M`
 
 # docker build -t project/react-web-midway
