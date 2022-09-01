2c82e4e3e8234c8ab36037ab134568f37088173c
Lvin Xu <lvin_xu@outlook.com>
2022-09-01 14:43:51 +0800

    build: update build.sh

--- docker/buildBaseNodeImage.sh
+++ docker/buildBaseNodeImage.sh
@@ -1,11 +1,13 @@
 [ -d ./node_modules ] && rm -rf ./node_modules
 
+git checkout . && git pull
+
 npm install
 
-version_tag = $(date +%Y.%m%d.%H%M)
+version_tag=$(date +%Y.%m%d.%H%M)
 
-docker build -t 175.24.166.118/react-web-midway/base_node_images:${version_tag} -f ./docker/Dockerfile.base_node_images .
+docker build -t 175.24.166.118/xulf/base_node_images:${version_tag} -f ./docker/Dockerfile.base_node_images .
 
-docker push 175.24.166.118/react-web-midway/base_node_images:${version_tag}
+docker push 175.24.166.118/xulf/base_node_images:${version_tag}
 
 echo 175.24.166.118/react-web-midway/base_node_images:${version_tag}
