63ba64aa14669c45123dcb8d500fc3f059b90a1a
Lvin Xu <lvin_xu@outlook.com>
2022-09-01 14:17:17 +0800

    build: update docker file

--- docker/buildImage.sh
+++ docker/buildImage.sh
@@ -3,9 +3,9 @@ git checkout . && git pull
 # 打包代码
 npm run build
 # harbor地址
-harbor_url = 175.24.166.118
+harbor_url=175.24.166.118
 # 镜像tag版本
-version_tag = $(date +%Y.%m%d.%H%M)
+version_tag=$(date +%Y.%m%d.%H%M)
 # 制作镜像
 docker build -t ${harbor_url}/react-web-midway/business_images/:${version_tag} -f ./docker/Dockerfile .
 
