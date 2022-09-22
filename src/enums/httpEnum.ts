2ce30f457816a335281cc77713dc39dcdd9ecccd
Lvin Xu <lvin_xu@outlook.com>
2022-09-22 16:12:02 +0800

    feat: add http enum

---
+++ src/enums/httpEnum.ts
@@ -0,0 +1,13 @@
+/**
+ * @description: Request result set
+ */
+export enum GlobalResultCodeEnum {
+  SUCCESS = 0,
+  ERROR = -1,
+  TIMEOUT = 401,
+}
+
+export enum GlobalResultMessageEnum {
+  SUCCESS = '成功！',
+  ERROR = '服务器错误！',
+}
